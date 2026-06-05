0266e8d087752bd8f7043e7f371227df78463afa
bhaskar241106 <bhaskar.24bcs7112@vitapstudent.ac.in>
2026-06-05 23:57:19 +0530

    Fix base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
-  base: '/SSVD-Constructions/',
+  base: '/',
   plugins: [
     ViteImageOptimizer({
       png: {
